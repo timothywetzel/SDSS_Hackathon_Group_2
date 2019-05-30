e35fe2b136fa77e8400eb75b8dade388e2209943
= <=>
2019-05-29 20:13:17 -0700

    Initial commit

---
+++ tw_exploratory_analysis.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+zillow_data = read_csv("../Desktop/sdss2019_data_hack/Datasets/king_zillow.csv")
+
+head(zillow_data)
+
+summary(zillow_data)
+
+eviction_data = read_csv("../Desktop/sdss2019_data_hack/Datasets/evictions.csv")
+
+head(eviction_data)
+
+summary(eviction_data)
+
