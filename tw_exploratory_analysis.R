59dd609ab037df20583ab97f767d548e43d764b1
= <=>
2019-05-30 07:44:04 -0700

    Added ACS data wrangling

--- tw_exploratory_analysis.R
+++ tw_exploratory_analysis.R
@@ -3,19 +3,45 @@ library(tidycensus)
 
 census_api_key("9a0425beda1e3422155a87028b50451b5483b2f2", install = TRUE)
 readRenviron("~/.Renviron")
-acs_data = get_acs(geography = "tract", year = 2013, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+acs_data_2010 = get_acs(geography = "tract", year = 2010, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+acs_data_2011 = get_acs(geography = "tract", year = 2011, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+acs_data_2012 = get_acs(geography = "tract", year = 2012, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+acs_data_2013 = get_acs(geography = "tract", year = 2013, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+acs_data_2014 = get_acs(geography = "tract", year = 2014, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+acs_data_2015 = get_acs(geography = "tract", year = 2015, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+acs_data_2016 = get_acs(geography = "tract", year = 2016, state = "WA", county = "King", geometry = TRUE, variables = "B19013_001")
+
+acs_data_2010 = subset(acs_data_2010, select = c(GEOID, NAME, variable, estimate, moe))
+acs_data_2011 = subset(acs_data_2011, select = c(GEOID, NAME, variable, estimate, moe))
+acs_data_2012 = subset(acs_data_2012, select = c(GEOID, NAME, variable, estimate, moe))
+acs_data_2013 = subset(acs_data_2013, select = c(GEOID, NAME, variable, estimate, moe))
+acs_data_2014 = subset(acs_data_2014, select = c(GEOID, NAME, variable, estimate, moe))
+acs_data_2015 = subset(acs_data_2015, select = c(GEOID, NAME, variable, estimate, moe))
+acs_data_2016 = subset(acs_data_2016, select = c(GEOID, NAME, variable, estimate, moe))
+within(acs_data_2016, rm("geometry"))
+colnames(acs_data_2016)
+
+write.csv(c(acs_data_2010, acs_data_2011, acs_data_2012, acs_data_2013, acs_data_2014, acs_data_2015, acs_data_2016), file = "../Desktop/sdss2019_data_hack/Datasets/acs_data_2010-2016.csv")
+head(acs_data_2010)
+
+read.csv("../Desktop/sdss2019_data_hack/Datasets/acs_data_2010-2016.csv")
+colnames(acs_data_2016)
+
+
+
+head(acs_data_2016)
+
+acs_data <- acs_data_2010 %>% select(-starts_with("geometry"))
 
 zillow_data = read_csv("../Desktop/sdss2019_data_hack/Datasets/king_zillow.csv")
-
 head(zillow_data)
-
 summary(zillow_data)
-
 ggplot(data = zillow_data, mapping = aes(x = ZHVI, y = ZRI)) + geom_point()
 
 eviction_data = read_csv("../Desktop/sdss2019_data_hack/Datasets/evictions.csv")
-
 head(eviction_data)
-
 summary(eviction_data)
 
+housing_inventory_data = readxl::read_xlsx("../Desktop/sdss2019_data_hack/Datasets/2018-Housing-Inventory-Count-Raw-File.xlsx")
+head(housing_inventory_data)
+summary(housing_inventory_data)
